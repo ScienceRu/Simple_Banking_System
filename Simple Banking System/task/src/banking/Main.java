8fbe170ef7bf9996725cf4a606c42ed63edc088f
ScienceRu <ivantorzhkov@gmail.com>
2021-09-28 20:52:38 +0300

    add check of pin and card number in sql DB

--- Simple Banking System/task/src/banking/Main.java
+++ Simple Banking System/task/src/banking/Main.java
@@ -1,10 +1,10 @@
 package banking;
 
-import static banking.Methods.createNewTable;
+
 
 public class Main {
     public static void main(String[] args) {
-        createNewTable();
+        CreateSqlDataBase.createNewTable();
         Methods user1 = new Methods();
         user1.choosingMenuItem();
 
--- Simple Banking System/task/src/banking/Methods.java
+++ Simple Banking System/task/src/banking/Methods.java
@@ -1,10 +1,5 @@
 package banking;
 
-import org.sqlite.SQLiteDataSource;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
@@ -18,10 +13,6 @@ public class Methods {
     private ArrayList<StringBuilder> userData = new ArrayList<>();
     private int menuItem;
     private static boolean isCorrect = false;
-
-
-    static String url = "jdbc:sqlite:C:/sqlite/banking.db";
-    public static int accountNumber = 1;
 
     Scanner sc = new Scanner(System.in);
 
@@ -81,57 +72,11 @@ public class Methods {
         return integerArrayList;
     }
 
-    public void setCardNumber() {
-
-        ArrayList<Integer> copyArrayListCardNumber = new ArrayList<>(useLuhnAlgorithm());
-        for (int i : copyArrayListCardNumber) {
-            sbCardNumber.append(i);
-        }
-//adding card number to sql base
-
-        //userData.add(sbCardNumber);
-
-        //creating pin code and adding it to sql base
-        Random random = new Random();
-        long accountIdentifier = random.nextInt(9999) + 1;
-        sbPinCode.append(accountIdentifier);
-        while (sbPinCode.length() < 4) {
-            sbPinCode.append("0", 0, 1);
-        }
-        //userData.add(sbPinCode);
-        insertDataToSQL(getCardNumber(), getPinCode());
-        System.out.println("Your card has been created");
-        System.out.println("Your card number:");
-        System.out.println(getCardNumber());
-
-        System.out.println("Your card PIN:");
-        System.out.println(getPinCode());
-
-        sbCardNumber = new StringBuilder("");
-        fullCardNumber = 400000000000000L;
-        sbPinCode = new StringBuilder("");
-//        accountNumber++;
-
-    }
 
     public StringBuilder getPinCode() {
         StringBuilder copySbPinCode = new StringBuilder("");
         copySbPinCode.append(sbPinCode);
         return copySbPinCode;
-    }
-
-    public void setPinCode() {
-        Random random = new Random();
-        long accountIdentifier = random.nextInt(9999) + 1;
-        //pinCode = accountIdentifier;
-        sbPinCode.append(accountIdentifier);
-        while (sbPinCode.length() < 4) {
-            sbPinCode.append("0", 0, 1);
-        }
-        userData.add(sbPinCode);
-        System.out.println("Your card PIN:");
-        System.out.println(getPinCode());
-        sbPinCode = new StringBuilder("");
     }
 
     public int getMenuItem() {
@@ -161,15 +106,31 @@ public class Methods {
     }
 
     void creatingAccount() {
-        setCardNumber();
-        setPinCode();
-        //for cheking consistency of array
-//        for (int i = 0; i < userData.length; i++) {
-//            for (int j = 0; j < userData[i].length; j++) {
-//                System.out.print(userData[i][j] + " ");
-//            }
-//        }
+        //creating card number
+        ArrayList<Integer> copyArrayListCardNumber = new ArrayList<>(useLuhnAlgorithm());
+        for (int i : copyArrayListCardNumber) {
+            sbCardNumber.append(i);
+        }
 
+        //creating pin code
+        Random random = new Random();
+        long accountIdentifier = random.nextInt(9999) + 1;
+        sbPinCode.append(accountIdentifier);
+        while (sbPinCode.length() < 4) {
+            sbPinCode.append("0", 0, 1);
+        }
+        //adding card number and pin to sql base
+        CreateSqlDataBase.insertDataToSQL(getCardNumber(), getPinCode());
+        //showing card number and pin to user
+        System.out.println("Your card has been created");
+        System.out.println("Your card number:");
+        System.out.println(getCardNumber());
+        System.out.println("Your card PIN:");
+        System.out.println(getPinCode());
+//dropping values to default for next iteration
+        sbCardNumber = new StringBuilder("");
+        fullCardNumber = 400000000000000L;
+        sbPinCode = new StringBuilder("");
         choosingMenuItem();
     }
 
@@ -179,7 +140,7 @@ public class Methods {
         System.out.println("Enter your PIN:");
         String inputOfUser2 = sc.next();
 
-        if (isValidCardNumberAndPinCode(inputOfUser1, inputOfUser2)) {
+        if (CreateSqlDataBase.isValidCardNumberAndPinCode(inputOfUser1, inputOfUser2)) {
             System.out.println("You have successfully logged in!");
             actingWithAccount();
         } else {
@@ -189,24 +150,24 @@ public class Methods {
     }
 
     //checking if card number and pin code are in array userData
-    private boolean isValidCardNumberAndPinCode(String s1, String s2) {
-
-        for (int i = 0, j = 1; j < userData.size(); ) {
-            if (s1.equals(userData.get(i).toString())) {
-                if (s2.equals(userData.get(j).toString())) {
-                    isCorrect = true;
-                    break;
-                } else {
-                    break;
-                }
-            } else {
-                i += 2;
-                j += 2;
-
-            }
-        }
-        return isCorrect;
-    }
+//    private boolean isValidCardNumberAndPinCode(String s1, String s2) {
+//
+//        for (int i = 0, j = 1; j < userData.size(); ) {
+//            if (s1.equals(userData.get(i).toString())) {
+//                if (s2.equals(userData.get(j).toString())) {
+//                    isCorrect = true;
+//                    break;
+//                } else {
+//                    break;
+//                }
+//            } else {
+//                i += 2;
+//                j += 2;
+//
+//            }
+//        }
+//        return isCorrect;
+//    }
 
     private void actingWithAccount() {
         System.out.println("1. Balance\r\n2. Log out\r\n0. Exit");
@@ -227,39 +188,8 @@ public class Methods {
         }
     }
 
-    public static void insertDataToSQL(StringBuilder s1, StringBuilder s2) {
-        SQLiteDataSource dataSource = new SQLiteDataSource();
-        dataSource.setUrl(url);
-        try (Connection connection = dataSource.getConnection()) {
-            try (Statement statement = connection.createStatement()) {
-                int i = statement.executeUpdate("INSERT INTO accounts (id, number) VALUES (" + accountNumber + ", '" + s1 + "');");
-                int j = statement.executeUpdate("UPDATE accounts SET pin = '" + s2 + "' WHERE id = " + accountNumber + ";");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 
-    public static void createNewTable() {
-        SQLiteDataSource dataSource = new SQLiteDataSource();
-        dataSource.setUrl(url);
-        try (Connection connection = dataSource.getConnection()) {
-            try (Statement statement = connection.createStatement()) {
 
-                statement.executeUpdate("CREATE TABLE IF NOT EXISTS accounts (" +
-                        "id INTEGER," +
-                        "number TEXT," +
-                        "pin TEXT," +
-                        "balance INTEGER DEFAULT 0)");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
 
-    }
 }
 
